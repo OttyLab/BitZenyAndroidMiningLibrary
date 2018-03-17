53794334603a8993c32ad1f3785e3d1a05daa007
ottylab <ottylab@gmail.com>
2018-03-17 15:41:00 +0900

    #12: library supports number of threads

--- app/src/main/cpp/libcpuminer/libcpuminer.h
+++ app/src/main/cpp/libcpuminer/libcpuminer.h
@@ -6,7 +6,7 @@ extern "C"{
 
 int init(void(*cb)(const char *format, va_list arg));
 int is_running();
-int start(const char* url, const char* user, const char* pass);
+int start(const char* url, const char* user, const char* pass, const int n_threads=0);
 int stop();
 
 #ifdef __cplusplus
